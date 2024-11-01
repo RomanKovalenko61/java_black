8ba39e333272de7e686a482c580175e110f9c834
Roman <astek14@mail.ru>
2024-11-01 17:44:14 +0300

    lesson 102: Channel и Buffer. Важные методы

---
+++ src/main/java/ionio/ChannelBufferEx2.java
@@ -0,0 +1,51 @@
+package ionio;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+public class ChannelBufferEx2 {
+    public static void main(String[] args) {
+        try (RandomAccessFile file = new RandomAccessFile("test5.txt", "r");
+             FileChannel channel = file.getChannel()) {
+
+            ByteBuffer buffer = ByteBuffer.allocate(5);
+            channel.read(buffer);
+            buffer.flip();
+            System.out.println((char) buffer.get()); //a
+            System.out.println((char) buffer.get()); //b
+            System.out.println((char) buffer.get()); //c
+            buffer.rewind();
+            System.out.println((char) buffer.get()); //a
+
+            System.out.println("----------------------------------");
+            buffer.compact();
+            channel.read(buffer);
+            buffer.flip();
+            while (buffer.hasRemaining()) {
+                System.out.println((char) buffer.get());
+            }
+
+            System.out.println("----------------------------------");
+            buffer.clear();
+            channel.read(buffer);
+            buffer.flip();
+            System.out.println((char) buffer.get()); // g
+            buffer.mark();
+            System.out.println((char) buffer.get()); // h
+            System.out.println((char) buffer.get()); // i
+            buffer.reset();
+            while (buffer.hasRemaining()) {
+                System.out.println((char) buffer.get());
+            }
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
