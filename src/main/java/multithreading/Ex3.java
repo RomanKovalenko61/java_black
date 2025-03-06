ddaa5ce0d1ed84f5b36a795855582eacc18b6bc6
Roman <astek14@mail.ru>
2025-03-06 17:41:00 +0300

    lesson 66: Варианты создания потоков

---
+++ src/main/java/multithreading/Ex3.java
@@ -0,0 +1,40 @@
+package multithreading;
+
+public class Ex3 implements Runnable {
+    @Override
+    public void run() {
+        for (int i = 0; i < 1000; i++) {
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        Thread thread = new Thread(new Ex3());
+        thread.start();
+
+        for (int i = 1000; i > 0; i--) {
+            System.out.println(i);
+        }
+
+//        Thread thread1 = new Thread(new MyThread3());
+//        Thread thread2 = new Thread(new MyThread4());
+//        thread1.start();
+//        thread2.start();
+    }
+}
+
+//class MyThread3 implements Runnable {
+//    public void run() {
+//        for (int i = 0; i < 1000; i++) {
+//            System.out.println(i);
+//        }
+//    }
+//}
+//
+//class MyThread4 implements Runnable {
+//    public void run() {
+//        for (int i = 1000; i > 0; i--) {
+//            System.out.println(i);
+//        }
+//    }
+//}
