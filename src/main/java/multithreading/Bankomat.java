969f62971ba592e5c7d8ff36c92daf55eec138de
Roman <astek14@mail.ru>
2025-03-08 20:12:49 +0300

    lesson 75: Lock и ReentrantLock

---
+++ src/main/java/multithreading/Bankomat.java
@@ -0,0 +1,45 @@
+package multithreading;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Bankomat {
+    public static void main(String[] args) throws InterruptedException {
+        Lock lock = new ReentrantLock();
+        new Employee("Zaur", lock);
+        new Employee("Oleg", lock);
+        new Employee("Elena", lock);
+        Thread.sleep(5000);
+        new Employee("Viktor", lock);
+        new Employee("Marina", lock);
+    }
+}
+
+class Employee extends Thread {
+    String name;
+    private Lock lock;
+
+    public Employee(String name, Lock lock) {
+        this.name = name;
+        this.lock = lock;
+        this.start();
+    }
+
+    public void run() {
+        if (lock.tryLock()) {
+            try {
+//            System.out.println(name + " wait");
+//            lock.lock();
+                System.out.println(name + " use ATM");
+                Thread.sleep(2000);
+                System.out.println(name + " finished");
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } finally {
+                lock.unlock();
+            }
+        } else {
+            System.out.println(name + " don't wait");
+        }
+    }
+}
