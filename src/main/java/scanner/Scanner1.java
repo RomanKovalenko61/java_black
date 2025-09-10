0afc94fd944ed9279567ee0e85ba3b6e5014d468
Roman <astek14@mail.ru>
2025-09-10 22:45:22 +0300

    lesson 117: class Scanner part 1

---
+++ src/main/java/scanner/Scanner1.java
@@ -0,0 +1,48 @@
+package scanner;
+
+import java.util.Scanner;
+
+public class Scanner1 {
+
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+
+//        System.out.println("Enter a number");
+//        int num = sc.nextInt();
+//        System.out.println("number is " + num);
+
+//        System.out.println("Enter two number");
+//        int x = sc.nextInt();
+//        int y = sc.nextInt();
+//        System.out.println("x / y = " + x / y);
+//        System.out.println("x % y = " + x % y);
+
+//        System.out.println("Enter some words");
+//        System.out.println("Enter two strings");
+//        String s1 = sc.nextLine();
+//        String s2 = sc.nextLine();
+//        System.out.println("You entered in 1st string: " + s1);
+//        System.out.println("You entered in 2nd string: " + s2);
+
+//        System.out.println("Enter string");
+//        String s = sc.next();
+//        System.out.println("You entered: " + s);
+
+//        System.out.println("Enter a floating point number");
+//        double num = sc.nextDouble();
+//        System.out.println("Num: " + num);
+
+        Scanner sc = new Scanner("Privet moy drug\nKak pojivaesh?\nCto xoroshego");
+//        String s = sc.next();
+//        System.out.println(s);
+
+//        String s1 = sc.nextLine();
+//        System.out.println(s1);
+
+//        while (sc.hasNextLine()) {
+//            System.out.println(sc.nextLine());
+//        }
+
+        System.out.println(sc.next().charAt(2));
+    }
+}
